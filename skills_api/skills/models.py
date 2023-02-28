e80e95538b8347634911a09f5600330a69012d07
James-10 <ockhuisjames@gmail.com>
2023-03-01 00:46:15 +0200

    Drill-Down fetaure for skills detail

--- skills_api/skills/models.py
+++ skills_api/skills/models.py
@@ -1,8 +1,12 @@
 from django.db import models
-
-class Skill(models.Model):
-    id = models.UUIDField(primary_key=True)
+    
+class Skills(models.Model):
+    id = models.AutoField(primary_key=True)
     skill_name = models.CharField(max_length=255)
+
+class SkillsDetail(models.Model):
+    id = models.AutoField(primary_key=True)
+    skill_name = models.ForeignKey(Skills, on_delete=models.CASCADE)
     description = models.TextField(blank=True)
     years_experience = models.SmallIntegerField()
 
--- skills_api/skills/urls.py
+++ skills_api/skills/urls.py
@@ -1,6 +1,9 @@
+from django.conf import settings
 from django.urls import path
-from .views import skill_list
+from django.conf.urls.static import static
+from .views import skill_list, skill_detail
 
 urlpatterns = [
     path('', skill_list, name='skill_list'),
-]
+    path('<int:skill_id>', skill_detail, name='skill_detail')
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
